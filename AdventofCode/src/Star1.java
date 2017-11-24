76286fbf43f632da470509c44606714feff2b431
kevin <kevin@DESKTOP-JFCDJ2B>
2017-11-24 11:59:54 -0500

    Completed Star 2 and added comments to Start 1 and Star 2

--- AdventofCode/src/Star1.java
+++ AdventofCode/src/Star1.java
@@ -1,23 +1,27 @@
 import java.io.*;
-import java.lang.Math.*;
 
 public class Star1 {
 	
+	// Function used to read the input file and determine the end location
 	public static int readInputFile(String file_name) {
-		String[] directions = {"North", "East", "South", "West"};
+		// current_direction = 0 --> North, current_direction = 1 --> East
+		// current_direction = 2 --> South, current_direction = 3 --> West
 		int current_direction = 0;
+		
+		// Used to keep track of the coordinates
 		int north = 0;
 		int east = 0;
 		
+		// Open the file and read through the movements
 		String line = null;
-		
 		try {
 			FileReader fr = new FileReader(file_name);
-			
 			BufferedReader br = new BufferedReader(fr);
 			line = br.readLine();
 	
+			// Split the input into separate directions and iterate through
 			for (String s : line.split(", ")) {
+				// Check which way you are turning and set the current_direction accordingly
 				switch (s.charAt(0)) {
 					case 'L': 	if (current_direction == 0)
 									current_direction = 3;
@@ -30,7 +34,8 @@ public class Star1 {
 									current_direction += 1;
 				  				break;	
 				}
-								
+				
+				// Get the amount of movement and change north and east accordingly
 				int movement = Integer.parseInt(s.split("[LR]")[1]);
 				switch(current_direction) {
 					case 0:		north += movement;
@@ -45,11 +50,11 @@ public class Star1 {
 			}
 			
 			br.close();
-			fr.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		// Return the distance (in blocks) from the start point to the headquarters
 		return Math.abs(north) + Math.abs(east);
 	}
 	
--- AdventofCode/src/Star2.java
+++ AdventofCode/src/Star2.java
@@ -1,23 +1,72 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
 
 public class Star2 {
 
+	// Function used to add all coordinates moving to next point into visited
+	public static String addIntoHash(HashMap<String, Integer> visited, int north, int east, int current_direction, int movement) {
+		
+		// Go from (north, east) to the next appropriate coordinate and add all visited points into visited
+		// Return any point that has been visited twice
+		switch(current_direction) {
+			case 0:		for (int i = 0; i < movement; i++) {
+							String insert_string = (north + i) + "," + east;
+							if (visited.containsKey(insert_string))
+								return insert_string;
+							visited.put(insert_string, 1);
+						}
+						break;
+			case 1:		for (int i = 0; i < movement; i++) {
+							String insert_string = north + "," + (east + i);
+							if (visited.containsKey(insert_string))
+								return insert_string;
+							visited.put(insert_string, 1);
+						}
+						break;
+			case 2:		for (int i = 0; i < movement; i++) {
+							String insert_string = (north - i) + "," + east;
+							if (visited.containsKey(insert_string))
+								return insert_string;
+							visited.put(insert_string, 1);
+						}
+						break;
+			case 3:		for (int i = 0; i < movement; i++) {
+							String insert_string = north + "," + (east - i);
+							if (visited.containsKey(insert_string))
+								return insert_string;
+							visited.put(insert_string, 1);
+						}
+						break;
+		}
+		
+		// Return null if no point has been crossed twice
+		return null;
+	}
+	
+	// Function used to read the input file and determine the end location
 	public static int readInputFile(String file_name) {
-		String[] directions = {"North", "East", "South", "West"};
+		// current_direction = 0 --> North, current_direction = 1 --> East
+		// current_direction = 2 --> South, current_direction = 3 --> West
 		int current_direction = 0;
+		
+		// Used to keep track of the coordinates
 		int north = 0;
 		int east = 0;
 		
-		String line = null;
+		// Used to keep track of all the coordinates that have been visited
+		HashMap<String, Integer> visited = new HashMap<String, Integer>();
 		
+		// Open the file and read through the movements
+		String line = null;
 		try {
 			FileReader fr = new FileReader(file_name);
-			
 			BufferedReader br = new BufferedReader(fr);
 			line = br.readLine();
-	
+
+			// Split the input into separate directions and iterate through
 			for (String s : line.split(", ")) {
+				// Check which way you are turning and set the current_direction accordingly
 				switch (s.charAt(0)) {
 					case 'L': 	if (current_direction == 0)
 									current_direction = 3;
@@ -30,8 +79,18 @@ public class Star2 {
 									current_direction += 1;
 				  				break;	
 				}
-								
+
+				
 				int movement = Integer.parseInt(s.split("[LR]")[1]);
+				
+				// Add the visited coordinates into visited and check if any place has been crossed twice. Return result if so.
+				String result = addIntoHash(visited, north, east, current_direction, movement);
+				if (result != null) {
+					br.close();
+					return Math.abs(Integer.parseInt(result.split(",")[0])) + Math.abs(Integer.parseInt(result.split(",")[1]));
+				}
+				
+				// Get the amount of movement and change north and east accordingly
 				switch(current_direction) {
 					case 0:		north += movement;
 								break;
@@ -45,12 +104,12 @@ public class Star2 {
 			}
 			
 			br.close();
-			fr.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		return Math.abs(north) + Math.abs(east);
+	
+		// Returns -1 if no point has been found that has been crossed twice
+		return -1;
 	}
 	
 	public static void main(String[] args) {
