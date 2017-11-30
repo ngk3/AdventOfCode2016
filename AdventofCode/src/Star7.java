9512b601be82e15129758b11f486cd8fda82fe40
kevin <kevin@DESKTOP-JFCDJ2B>
2017-11-29 22:50:28 -0500

    Wrote most of the determine_five_most_common_letters function in Star7. Will need to test this function.

--- AdventofCode/src/Star7.java
+++ AdventofCode/src/Star7.java
@@ -1,5 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class Star7 {
 	
@@ -21,9 +25,29 @@ public class Star7 {
 		}
 	}
 	
-	public static String[] determine_five_most_common_letters(String encrypt) {
-		//String[] characters = new String[];
-		return new String[10];
+	public static Integer[] determine_five_most_common_letters(String encrypt) {
+		Integer[] five_most_common = new Integer[5];
+		
+		HashMap<Integer, Integer> characters = new HashMap<Integer, Integer>();
+		for(int i = 0; i < 26; i++)
+			characters.put(i, 0);
+		
+		for(int i = 0; i < encrypt.length(); i++)
+			characters.put(Character.getNumericValue(encrypt.charAt(i)) - 10, characters.get(Character.getNumericValue(encrypt.charAt(i)) - 10) + 1);
+		
+		ArrayList<Integer> frequency = (ArrayList<Integer>) characters.values();
+		Collections.sort(frequency);
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 26; j++) {
+				if (characters.get(j) == frequency.get(i)) {
+					five_most_common[i] = j;
+					characters.put(j, -1);
+				}
+			}
+		}
+		
+		return five_most_common;
 	}
 	
 	public static boolean check_match(String encrypt, String checksum) {
@@ -31,11 +55,11 @@ public class Star7 {
 	}
 	
 	public static void main(String[] argvs) {
-		System.out.println(Character.getNumericValue('z'));
+		System.out.println(Character.getNumericValue('a'));
 		System.out.println((char)65);
-		for(int i = 0; i < 26; i++) {
+		/*for(int i = 0; i < 26; i++) {
 			System.out.println((char)(65 + i));
-		}
+		}*/
 		// Characters are from 65 to 90
 	}
 }
